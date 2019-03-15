6f0311e4a454852bf041c04bbb6dacc1bf359310
raccooccoon <zavadskaya_91@mail.ru>
2019-03-15 22:47:20 +0300

    #1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig = [arr1]) {
+  if (arr1.length == 1) {
+    var arr2 = [...arr1];
+    var countA = 0;
+    var countB = 0;
+    for (var i = 0; i < str.length; i++) {
+      if (str[i] == arr2[0]) countA++;
+      if (str[i] == arr[1]) countB++;
+    }
+    if (countA == countB) return true;
+  }
 }

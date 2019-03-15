fb7af913506f1e67cea6e38a05efb9b9ac1fa211
raccooccoon <zavadskaya_91@mail.ru>
2019-03-16 01:10:15 +0300

    #4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-module.exports = function check(str, bracketsConfig = [arr1]) {
-  if (arr1.length == 1) {
-    var arr2 = [...arr1];
-    var countA = 0;
-    var countB = 0;
-    for (var i = 0; i < str.length; i++) {
-      if (str[i] == arr2[0]) countA++;
-      if (str[i] == arr2[1]) countB++;
+module.exports = function check(str, bracketsConfig) {
+
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    if (i[0] = '(') var re = /\(\)/;
+    if (i[0] ='{') var re = /\{\}/;
+    if (i[0] = '[') var re = /\[\]/;
+    if (i[0] = '|') var re = /\|\|/;
+    while (str.search(re) != -1) {
+      str = str.replace(re, '');
     }
-    if (countA == countB) return true;
+
+    return str;
+    }
+    if (str = '') return true;
   }
-  return true;
-}
+ 

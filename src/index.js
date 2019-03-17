3598456c505c482d9a47d9e7dbaebf44bc6a2472
raccooccoon <zavadskaya_91@mail.ru>
2019-03-17 21:33:10 +0300

    Fiiinaly

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ module.exports = function check(str, bracketsConfig) {
 
   var arrNew = bracketsConfig.map( element => element.join('') );
     while (arrNew.some (v => str.includes(v)) ) {
-
-    for (var i = 0; i < arrNew.length; i++) {
-      do {
-        str = str.replace(arrNew[i], '');
-      } while (str.indexOf(arrNew[i]) != -1);
+      for (var i = 0; i < arrNew.length; i++) {
+        do {
+          str = str.replace(arrNew[i], '');
+        } while (str.indexOf(arrNew[i]) != -1);
+      }
     }
-
-  }
-
-    return str.length === 0 ? true : false;
-  }
+  return str.length === 0 ? true : false;
+}
 
     
 

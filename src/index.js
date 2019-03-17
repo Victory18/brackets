28f3f4a388a3a0589b09342810b97d9a670ac01d
raccooccoon <zavadskaya_91@mail.ru>
2019-03-17 18:33:26 +0300

    17

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ module.exports = function check(str, bracketsConfig) {
   var arrNew = bracketsConfig.map( element => element.join('') );
 
   for (var i = 0; i < arrNew.length; i++) {
-    str = str.replace(arrNew[i], '');
-    if (str.indexOf(arrNew[i]) == -1) continue;
-    else str = str.replace(arrNew[i], '');
-    }
+    do {
+      str = str.replace(arrNew[i], '');
+    } while (str.indexOf(arrNew[i]) != -1);
 
+  }
+  
   return str.length === 0 ? true : false;
 
 }

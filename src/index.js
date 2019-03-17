83a06a0448ab44c60c14131c3a9c151b3a023d24
raccooccoon <zavadskaya_91@mail.ru>
2019-03-17 21:04:15 +0300

    19

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
 
   var arrNew = bracketsConfig.map( element => element.join('') );
-  var re = new RegExp(arrNew.join('|'));
 
-  do {
     
     for (var i = 0; i < arrNew.length; i++) {
       do {
@@ -11,7 +9,6 @@ module.exports = function check(str, bracketsConfig) {
       } while (str.indexOf(arrNew[i]) != -1);
     }
 
-  } while (str.search(re) == 1);
     return str.length === 0 ? true : false;
   }
 

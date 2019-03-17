599602b3d8a4c7fa6357b9efd968d5a7d4df7549
raccooccoon <zavadskaya_91@mail.ru>
2019-03-17 16:31:39 +0300

    5

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ module.exports = function check(str, bracketsConfig) {
   for (var i = 0; i < arrNew.length; i++) {
     var re = new RegExp('i='+i, 'g');
     str = str.replace(re, '');
+    if (str.search(re) == -1) break;
+    else str = str.replace(re, '');
   }
 
   return true ? str.length == 0 : false;

65fbee10e976ee23d476d2f24c7be9fa4c2484db
raccooccoon <zavadskaya_91@mail.ru>
2019-03-17 18:08:33 +0300

    114

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
 
-  var arrNew = bracketsConfig.map( element => element == element.join('') );
+  var arrNew = bracketsConfig.map( element => element.join('') );
 
   for (var i = 0; i < arrNew.length; i++) {
     var el = arrNew[i];
-    var re = new RegExp('${el}', 'g');
-    str = str.replace(re, '');
-    if (str.search(re) == -1) break;
-    else str = str.replace(re, '');
+    var re = new RegExp(el, 'g');
+    var newStr = str.replace(re, '');
+    }
+
+    if (newStr.search(re) == -1) break;
   }
 
   return str.length === 0 ? true : false;

204e48e8bb152e099314a5efd8059f98d958813c
raccooccoon <zavadskaya_91@mail.ru>
2019-03-17 18:58:31 +0300

    17

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ module.exports = function check(str, bracketsConfig) {
 
   var arrNew = bracketsConfig.map( element => element.join('') );
 
-  for (var i = 0; i < arrNew.length; i++) {
-    do {
-      str = str.replace(arrNew[i], '');
-    } while (str.indexOf(arrNew[i]) != -1);
-
+  function search () {
+    for (var i = 0; i < arrNew.length; i++) {
+      do {
+        str = str.replace(arrNew[i], '');
+      } while (str.indexOf(arrNew[i]) != -1);
+    }
+    return str.indexOf(arrNew[i]) == -1 ? str : search();
   }
-  
+
   return str.length === 0 ? true : false;
 
 }

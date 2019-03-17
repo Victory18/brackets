bac3b664b870706e404dccf184f16b009e08807a
raccooccoon <zavadskaya_91@mail.ru>
2019-03-17 17:19:59 +0300

    11

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = function check(str, bracketsConfig) {
   var arrNew = bracketsConfig.map( element => element == element.join('') );
 
   for (var i = 0; i < arrNew.length; i++) {
-    var re = new RegExp(i, 'g');
+    var re = new RegExp(arrNew[i], 'g');
     str = str.replace(re, '');
     if (str.search(re) == -1) break;
     else str = str.replace(re, '');
